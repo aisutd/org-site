358c1e1332c1ff1a25505aabe251fec1dcba2a4d
Joshua Baker <joshuakb2@gmail.com>
2018-05-01 19:09:53 -0500

    Stopped using a deprecated function call and updated the serveLocal.sh script to help people install dependencies.

--- serveLocal.sh
+++ serveLocal.sh
@@ -1,3 +1,25 @@
 #!/bin/sh
 
-bundle exec jekyll serve
+help() {
+    echo 'You may need to install dependencies.'
+    echo 'Try executing the following command:'
+    echo 'sudo apt install ruby ruby-bundler ruby-dev zlib1g zlib1g-dev && bundle install'
+    echo
+    echo 'If you run into an error about permission denied, try `sudo chmod -R a+w /var/lib/gems/*` and then try `bundle install` again.'
+}
+
+if command -v bundle >/dev/null 2>&1; then
+    bundle exec jekyll serve
+    case $? in
+        0|130)
+            exit 0
+            ;;
+        *)
+            help
+            exit 1
+            ;;
+    esac
+else
+    help
+    exit 1
+fi
