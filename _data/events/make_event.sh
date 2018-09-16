dd7509a14ea3437f0476b7b519fb4dfeb9ae7700
Joshua Baker <joshuakb2@gmail.com>
2018-09-16 17:26:19 -0500

    Added Sept 20 Intro to Deep Learning workshop info.
    Added make_event.sh script to make it easier to create new event YAML files.
    
    Removed event/type/old folders because we decided a while back not to use them anyway...
    
    Added --public option to serveLocal.sh to serve the site such that other LAN devices can access it.
    Modified _includes/base_path to use absolute paths in the form of /root/dir instead of http://127.0.0.1/root/dir,
    as the previous functionality broke website access from other LAN devices.

---
+++ _data/events/make_event.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+eventTypes=("talk" "workshop" "competition")
+
+read -p 'Event name (no double quotes): ' name
+read -p 'Date: ' date
+read -p 'Time: ' time
+read -p 'Location: ' location
+read -p 'Location link: ' link
+
+PS3='What type of event is this? '
+select opt in "${eventTypes[@]}"; do
+    for f in "${eventTypes[@]}"; do
+        if [[ "$opt" == "$f" ]]; then
+            eventType="$opt"
+            break
+        elif [[ "$REPLY" == "$f" ]]; then
+            eventType="$REPLY"
+            break
+        fi
+    done
+
+    if [[ "$eventType" != "" ]]; then
+        break
+    fi
+done
+read -p 'Filename (avoid spaces) (without extension): ' filename
+
+index=$(( 1 + $(grep -R 'index: ' "$DIR/${eventType}s/"* | cut -d: -f3 | sort -rn | head -n 1) ))
+
+(
+    echo 'name: "'"$name"'"'
+    echo 'date: '"$date"
+    echo 'time: '"$time"
+    echo 'location: '"$location"
+    echo 'location_link: '"$link"
+    echo 'category: '"$eventType"
+    echo 'index: '"$index"
+    echo 'description: '
+) >"$DIR/${eventType}s/$filename.yml"
+
+nano +8,14 "$DIR/${eventType}s/$filename.yml"
