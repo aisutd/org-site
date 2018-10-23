5ce9c7c64ee9d17362c8ae2015413111d1129e7f
Joshua Baker <joshuakb2@gmail.com>
2018-10-23 15:26:00 -0500

    Updated the list of dependencies in serveLocal.sh

--- serveLocal.sh
+++ serveLocal.sh
@@ -6,7 +6,7 @@ help() {
     echo 'You may need to install dependencies.'
     echo 'Try executing the following commands:'
     echo -e '\tcd "'"$(pwd)"'"'
-    echo -e '\tsudo apt install ruby ruby-bundler ruby-dev zlib1g zlib1g-dev && bundle install'
+    echo -e '\tsudo apt install ruby ruby-bundler ruby-dev zlib1g zlib1g-dev make gcc g++ && bundle install'
     echo
     echo 'If you run into an error about permission denied, try `sudo chmod -R a+w /var/lib/gems/*` and then try `bundle install` again.'
 }
