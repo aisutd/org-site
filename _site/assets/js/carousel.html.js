679c81abe1edb45bf59cf75524f09154122ddc5f
Anirudh Emmadi <kanna6501@gmail.com>
2020-08-25 18:24:17 -0500

    Old Site Data

---
+++ _site/assets/js/carousel.html.js
@@ -0,0 +1,9 @@
+
+// Set up the carousel
+jQuery(document).ready(function($) {
+    $('#featured-images').owlCarousel({
+      autoPlay : true,
+      pagination : false,
+      items : 3
+    });
+});
