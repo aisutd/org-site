873ddd07654544c617b923fa8dac0ab44a11d61c
Willie Chalmers III <willie@aisociety.io>
2021-03-07 11:43:02 -0600

    Add linter and dependencies
    This project will use Prettier to automate linting. It now has a lint
    script (npm run lint) to clean up code style.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,25 +1,30 @@
 module.exports = {
+  parser: '@typescript-eslint/parser', // Specifies the ESLint parser
+  parserOptions: {
+    ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
+    sourceType: 'module', // Allows for the use of imports
+    ecmaFeatures: {
+      jsx: true, // Allows for the parsing of JSX
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect', // Tells eslint-plugin-react to automatically detect the version of React to use
+    },
+  },
   env: {
     browser: true,
     es2021: true,
     node: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    'react',
-    '@typescript-eslint',
+    'plugin:react/recommended', // Uses the recommended rules from @eslint-plugin-react
+    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   rules: {
+    // Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
+    // See https://github.com/yannickcr/eslint-plugin-react/issues/2440#issuecomment-698553263
+    'react/react-in-jsx-scope': 'off',
   },
 };
