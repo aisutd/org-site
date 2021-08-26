14637e17f0290c696c170676b263575be74dbea6
KumailB <sxb180119@utdallas.edu>
2021-08-26 02:10:26 -0500

    Improved events grid

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -18,6 +18,7 @@ export default function EventsPage({ events }: EventsPageProps) {
   const futureEvents: Event[] = [];
   const onGoingEvents: Event[] = [];
   const pastEvents: Event[] = [];
+  const pastEventsCols = [[], [], []];
 
   events.forEach(function (eachEvent) {
     const startTime = new Date(eachEvent.startDate);
@@ -28,6 +29,17 @@ export default function EventsPage({ events }: EventsPageProps) {
     else onGoingEvents.push(eachEvent);
   });
 
+  const pastEventCards = pastEvents.map((event) => {
+    return (
+      <div key={event.id} className="">
+        <FeatureEvent key={event.id} event={event} />
+      </div>
+    );
+  });
+
+  for (let i = 0; i < pastEventCards.length; i++) {
+    pastEventsCols[i % 3].push(pastEventCards[i]);
+  }
   const futureEventCards = futureEvents.map((event) => {
     return <FeatureEvent key={event.id} event={event} />;
   });
@@ -36,14 +48,25 @@ export default function EventsPage({ events }: EventsPageProps) {
     return <FeatureEvent key={event.id} event={event} onGoing={true} />;
   });
 
+  let pastEventsDiv;
+  if (pastEvents.length == 0) {
+    pastEventsDiv = <div>There are no past events</div>;
+  } else {
+    pastEventsDiv = (
+      <div className="flex flex-row">
+        <div className="flex flex-col w-1/3">{pastEventsCols[0]}</div>
+        <div className="flex flex-col w-1/3">{pastEventsCols[1]}</div>
+        <div className="flex flex-col w-1/3">{pastEventsCols[2]}</div>
+      </div>
+    );
+  }
+
   let upComingEventDiv;
   if (futureEvents.length == 0) {
     upComingEventDiv = <div>No upcoming events as of yet, Please check back again!</div>;
   } else {
     upComingEventDiv = (
-      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 items-start">
-        {futureEventCards}
-      </div>
+      <div className="grid grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">{futureEventCards}</div>
     );
   }
 
@@ -58,9 +81,6 @@ export default function EventsPage({ events }: EventsPageProps) {
       </section>
     );
   }
-  const pastEventCards = pastEvents.map((event) => {
-    return <EventItem key={event.id} event={event} />;
-  });
 
   return (
     <div>
@@ -102,7 +122,7 @@ export default function EventsPage({ events }: EventsPageProps) {
         <section className="bg-ais-light-gray py-8">
           <div className="mx-auto max-w-sm sm:max-w-md md:max-w-lg lg:max-w-2xl xl:max-w-4xl 2xl:max-w-6xl py-2">
             <div className="text-3xl font-bold mb-4">Event Archive</div>
-            <div>{pastEventCards}</div>
+            {pastEventsDiv}
           </div>
         </section>
       </main>
